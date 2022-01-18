d64855c7cc3c86a482d3ebd71c3e7d6637833203
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-01-17 22:50:25 -0500

    form sumbit function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function() { 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+var createTaskHandler = function(event) {
 
-buttonEl.addEventListener("click", createTaskHandler);
+    event.preventDefault();
+  
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
